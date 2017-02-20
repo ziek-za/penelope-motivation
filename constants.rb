08454539be5f4be7abde3a13d0a2c83ac93841ec
Darryn Papathanasiou <darryn.papathanasiou@ip-10-0-19-65.eu-west-1.compute.internal>
2017-02-20 17:56:47 +0200

    _Added functionality to penelope.

---
+++ constants.rb
@@ -0,0 +1,758 @@
+SUBJECTS = %w(
+  Braeme\ Leroux
+  Bosco
+  Bee
+  Awb
+  Aveshan
+  Anja\ Vandenberg
+  Andrew\ Butchart
+  Andrea\ Oxenham
+  Adrian\ Linde
+  Adam
+  Collin
+  Cornevandyk
+  Cowley
+  Dante
+  Darryn\ Papathanasiou
+  Davy
+  Deanen\ Reddy
+  Denys\ Stroebel
+  Divashen
+  Ed\ symons
+  Claudina
+  Casper
+  Brayoni
+  Braeme\ Leroux
+  Bosco
+  Bee
+  Emile\ Alberts
+  Eduard
+  Edmore\ Moyo
+  Gerhard\ Engelbrecht
+  Francois\ Retief
+  Eugene\ Nieuwenhuizen
+  Erik\ Botes
+  Emile\ Alberts
+  Eduard
+  Edmore\ Moyo
+  Divashen
+  Denys\ Stroebel
+  Hawa
+  James\ Nzomo
+  Jason\ Murphy
+  Jon\ Gore
+  Jumo
+  Jessie\ Hurst
+  Darryn\ Papathanasiou
+  Davy
+  Jessie\ Hurst
+  Jon\ Gore
+  Justipops
+  Kate\ T
+  Kenneth
+  Kirsten\ Clacey
+  Komuw
+  Kurt
+  Kyle\ Patience
+  Jason\ Murphy
+  James\ Nzomo
+  Hawa
+  Louise\ Perold
+  Luke\ Abrahams
+  Lisa\ Morrison
+  Lawrence
+  Leana
+  Mwas
+  Nic
+  Nicolv
+  Nigel\ Cowling
+  Nigel\ Pedro
+  Obat
+  Okal
+  Rhiannon\ Liebowitz
+  Peter\ Matogo
+  Michael\ Bester
+  Max\ Kleynhans
+  Martin\ Schramm
+  Russell\ Jarvis
+  Rayna\ Leibrandt
+  Louise\ Perold
+  Marty
+  Quintis\ Venter
+  Luke\ Abrahams
+  Shaun\ Moodley
+  Stephenm
+  Steve
+  Teddybear
+  Tony\ Mamacos
+  Vanessa\ Fisher
+  Vivek\ Bhimma
+  Pierre\ Hugo
+  Rayna\ Leibrandt
+  Walter\ Proske
+  Sean\ Oreilly
+  Samuel\ Alfred
+  Quintis\ Venter
+  Russell\ Jarvis
+  Sam
+  Yunus\ Nordien
+  Rhiannon\ Liebowitz
+  Wing
+).freeze
+
+VERBS = %w(
+  accepts
+  adds
+  admires
+  admits
+  advises
+  affords
+  agrees\ to
+  alerts
+  allows
+  amuses
+  analyses
+  announces\ to
+  annoys
+  answers
+  apologises\ to
+  appears\ to
+  applauds
+  appreciates
+  approves
+  argues\ with
+  arrests
+  arrives\ with
+  asks
+  attaches
+  attacks
+  attracts
+  avoids
+  bakes
+  balances
+  bans
+  bangs
+  bares\ with
+  bats
+  bathes
+  battles
+  beams
+  begs
+  behaves\ with
+  belongs\ to
+  bleachs
+  blesses
+  blinds
+  blinks\ at
+  blushes\ at
+  boasts\ to
+  boils
+  bombs
+  bores
+  borrows
+  bounces
+  bows
+  brakes
+  breathes
+  bruises
+  brushes
+  bubbles
+  bumps
+  burns
+  burys
+  buzzs
+  calculates
+  calls
+  camps
+  cares\ about
+  carries
+  carves
+  challenges
+  changes
+  charges
+  chases
+  cheats\ on
+  checks\ with
+  cheers
+  chews
+  chokes
+  chops
+  claims
+  claps
+  cleans
+  clears
+  clips
+  closes
+  coachs
+  coils
+  collects
+  colours
+  combs
+  commands
+  communicates
+  compares
+  competes
+  complains
+  completes
+  concentrates
+  concerns
+  confesses
+  confuses
+  connects
+  considers
+  consists
+  contains
+  continues
+  copys
+  corrects
+  coughs
+  counts
+  covers
+  cracks
+  crashs
+  crawls
+  crosses
+  crushs
+  crys
+  cures
+  curls
+  curves
+  cycles
+  dams
+  damages
+  dances
+  dares
+  decays
+  deceives
+  decides
+  decorates
+  delays
+  delights
+  delivers
+  depends
+  describes
+  deserts
+  deserves
+  destroys
+  detects
+  develops
+  disagrees
+  disappears
+  disapproves
+  disarms
+  discovers
+  dislikes
+  divides
+  doubles
+  doubts
+  drags
+  drains
+  dreams
+  dresses
+  drips
+  drops
+  drowns
+  drums
+  drys
+  dusts
+  earns
+  educates
+  embarrasses
+  employs
+  emptys
+  encourages
+  ends
+  enjoys
+  enters
+  entertains
+  escapes
+  examines
+  excites
+  excuses
+  exercises
+  exists
+  expands
+  expects
+  explains
+  explodes
+  extends
+  faces
+  fades
+  fails
+  fancys
+  fastens
+  faxs
+  fears
+  fences
+  fetchs
+  files
+  fills
+  films
+  fires
+  fits
+  fixs
+  flaps
+  flashs
+  floats
+  floods
+  flows
+  flowers
+  folds
+  follows
+  fools
+  forces
+  forms
+  founds
+  frames
+  frightens
+  frys
+  gathers
+  gazes
+  glows
+  glues
+  grabs
+  grates
+  greases
+  greets
+  grins
+  grips
+  groans
+  guarantees
+  guards
+  guesses
+  guides
+  hammers
+  hands
+  handles
+  hangs
+  happens
+  harasses
+  harms
+  hates
+  haunts
+  heads
+  heals
+  heaps
+  heats
+  helps
+  hooks
+  hops
+  hopes
+  hovers
+  hugs
+  hums
+  hunts
+  hurrys
+  identifys
+  ignores
+  imagines
+  impresses
+  improves
+  includes
+  increases
+  influences
+  informs
+  injects
+  injures
+  instructs
+  intends
+  interests
+  interferes
+  interrupts
+  introduces
+  invents
+  invites
+  irritates
+  itchs
+  jails
+  jams
+  jogs
+  joins
+  jokes
+  judges
+  juggles
+  jumps
+  kicks
+  kills
+  kisses
+  kneels
+  knits
+  knocks
+  knots
+  labels
+  lands
+  lasts
+  laughs\ at
+  launches\ at
+  learns
+  levels
+  licenses
+  licks
+  lies
+  lightens
+  likes
+  lists
+  listens
+  lives
+  loads
+  locks
+  longs\ for
+  looks\ at
+  loves
+  manages
+  marks
+  marries
+  matches
+  mates\ with
+  matters\ to
+  measures
+  meddles
+  melts
+  memorises
+  mends
+  mess ups
+  milks
+  mines
+  misses
+  mixes
+  moans\ to
+  mourns\ over
+  moves
+  muddles
+  mugs
+  multiplies\ with
+  murders
+  nails
+  names
+  needs
+  nests
+  nods\ at
+  notes
+  notices
+  numbers
+  obeys
+  objects
+  observes
+  obtains
+  occurs
+  offends
+  offers
+  opens
+  orders
+  overflows
+  owes
+  owns
+  packs
+  paddles
+  paints
+  parks
+  parts
+  passes
+  pastes
+  pats
+  pecks
+  pedals\ with
+  peels
+  peeps\ at
+  permits
+  phones
+  picks
+  pinches
+  places
+  plans\ with
+  plants
+  plays\ with
+  pleases
+  plugs
+  points
+  pokes
+  polishs
+  pops
+  possesses
+  posts
+  pours
+  practises
+  prays
+  preachs
+  precedes
+  prefers
+  prepares
+  presents
+  preserves
+  presses
+  pretends
+  prevents
+  pricks
+  prints
+  produces
+  programs
+  promises
+  protects
+  provides
+  pulls
+  pumps
+  punchs
+  punctures
+  punishs
+  pushs
+  questions
+  queues
+  races
+  radiates
+  rains
+  raises
+  reachs
+  realises
+  receives
+  recognises
+  records
+  reduces
+  reflects
+  refuses
+  regrets
+  reigns
+  rejects
+  rejoices
+  relaxs
+  releases
+  relys
+  remains
+  remembers
+  reminds
+  removes
+  repairs
+  repeats
+  replaces
+  replys
+  reports
+  reproduces
+  requests
+  rescues
+  retires
+  returns
+  rhymes
+  rinses
+  risks
+  robs
+  rocks
+  rolls
+  rots
+  rubs
+  ruins
+  rules
+  rushs
+  sacks
+  sails
+  satisfys
+  saves
+  saw
+  scares
+  scatters
+  scolds
+  scorchs
+  scrapes
+  scratchs
+  screams
+  screws
+  scribbles
+  scrubs
+  seals
+  searches
+  separates
+  serves
+  settles
+  shades
+  shares
+  shaves
+  shelters
+  shivers
+  shocks
+  shops
+  shrugs
+  sighs§
+  signs
+  signals
+  sins
+  sips
+  skis
+  skips
+  slaps
+  slips
+  slows
+  smashs
+  smells
+  smiles
+  smokes
+  snatchs
+  sneezes
+  sniffs
+  snores
+  snows
+  soaks
+  soothes
+  sounds
+  spares
+  sparks
+  sparkles
+  spells
+  spills
+  spoils
+  spots
+  sprays
+  sprouts
+  squashs
+  squeaks
+  squeals
+  squeezes
+  stains
+  stamps
+  stares
+  starts
+  stays
+  steers
+  steps
+  stirs
+  stitchs
+  stops
+  stores
+  straps
+  strengthens
+  stretchs
+  strips
+  strokes
+  stuffs
+  subtracts
+  succeeds
+  sucks
+  suffers
+  suggests
+  suits
+  supplys
+  supports
+  supposes
+  surprises
+  surrounds
+  suspects
+  suspends
+  switchs
+  talks\ about
+  tames
+  taps
+  tastes
+  teases
+  telephones
+  tempts
+  terrifys
+  tests
+  thanks
+  thaws
+  ticks
+  tickles
+  ties
+  times
+  tips
+  tires
+  touches
+  tows
+  traces
+  trades
+  trains
+  transports
+  traps
+  travels\ aside
+  treats
+  trembles\ near
+  tricks
+  trips\ over
+  trots
+  troubles
+  trusts
+  trys
+  tugs
+  tumbles
+  turns
+  twists
+  types
+  undresses
+  unfastens
+  unites
+  unlocks
+  unpacks
+  untidys
+  uses
+  vanishes\ without
+  visits
+  wails
+  waits
+  walks
+  wanders
+  wants
+  warms
+  warns
+  washs
+  wastes
+  watchs
+  waters
+  waves
+  weighs
+  welcomes
+  whines
+  whips
+  whirls
+  whispers
+  whistles
+  winks
+  wipes
+  wishs
+  wobbles
+  wonders
+  works
+  worrys\ about
+  wraps
+  wrecks
+  wrestles
+  wriggles
+  x-rays
+  yawns\ around
+  yells\ at
+  zooms\ in\ on
+).freeze
+
+PRONOUNS = %w(
+  you
+  your\ dog
+  your\ cat
+  your\ dog,\ terry,
+  your\ family
+).freeze
+
+NOUNS = %w(
+  goodluck
+  love
+  pain
+  emotion
+  feelings
+).freeze
+
+ADJECTIVES = %w(
+  non-stop
+  before\ the\ end\ of\ the\ day
+  each\ time\ you\ sitdown
+  while\ you\ talk
+  when\ you\ leave\ the\ room
+  before\ you\ arrive
+  during\ the\ day
+  10\ times\ a\ day
+  constantly
+  every\ Friday\ evening
+  on\ Wednesday
+  on\ Thursday
+  on\ Friday
+  on\ Saturday
+  on\ Sunday
+  on\ Monday
+  on\ Monday\ morning
+  on\ Monday\ afternoon
+  while\ you\ work
+  while\ gymming
+).freeze
