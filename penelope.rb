95440a589f195c318bcec7cdd8befb26e6bb78a4
Samuel Alfred <samuel.alfred@ip-10-0-19-84.eu-west-1.compute.internal>
2017-03-01 15:55:26 +0200

    Removed tabs

--- penelope.rb
+++ penelope.rb
@@ -1,22 +1,21 @@
-	require_relative 'constants'
- 
-	output = []
- 
-	# Select Subjects
-	output << SUBJECTS[rand SUBJECTS.length]
-	# Select the verb
-	output << VERBS[rand VERBS.length]
-	# Randomize between a pro-noun or another subject
-	case rand(2)
-	when 0
-	  output << PRONOUNS[rand PRONOUNS.length]
-	when 1
-	  output << SUBJECTS[rand SUBJECTS.length]
-	end
-	# output << NOUNS[rand NOUNS.length]
-	# Select an adjective
-	output << ADJECTIVES[rand ADJECTIVES.length]
- 
-	# Display the output
-	p output.join(' ').concat('.')
- 
+require_relative 'constants'
+
+output = []
+
+# Select Subjects
+output << SUBJECTS[rand SUBJECTS.length]
+# Select the verb
+output << VERBS[rand VERBS.length]
+# Randomize between a pro-noun or another subject
+case rand(2)
+when 0
+  output << PRONOUNS[rand PRONOUNS.length]
+when 1
+  output << SUBJECTS[rand SUBJECTS.length]
+end
+# output << NOUNS[rand NOUNS.length]
+# Select an adjective
+output << ADJECTIVES[rand ADJECTIVES.length]
+
+# Display the output
+p output.join(' ').concat('.')
